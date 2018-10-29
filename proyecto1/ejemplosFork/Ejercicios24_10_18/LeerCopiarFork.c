0932de7c9ad8b3922073da0071040800c51da05b
199332 <199332@iessierradeguara.com>
2018-10-29 20:29:01 +0100

    trabajo en el fork para leer y copiar

--- proyecto1/ejemplosFork/Ejercicios24_10_18/LeerCopiarFork.c
+++ proyecto1/ejemplosFork/Ejercicios24_10_18/LeerCopiarFork.c
@@ -1,78 +1,86 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {  
+    //variable para crear la tubería el 0 lee y el 1 escribe 
     int padreHijo[2];
-
+    //creo tuberia para mandar los datos
+    pipe(padreHijo);
+    //variable para saber en el fork si es el hijo o el padre
     pid_t pid;
     
      //creo una variable de tipo FILE con la que habriré el archivo
     FILE *archivo;
     FILE *archivoNuevo;
+    archivo = fopen("texto.txt","r");
+    archivoNuevo = fopen("texto_fork.txt","w"); 
     
-    //comienzo los dos procesos
-    //se puede poner el fork()solo?
+  //comienzo los dos procesos
     pid = fork();
     
-   if (pid == 0) {
-    printf("Hola soy el hijo %d\n",getpid()); 
-    sleep(1);
-    wait(NULL);
-
- }else 
- if (pid == -1) {
+   if (pid == 0) {//PROCESO HIJO recibe caracter y escribe en el fichero nuevo
+    printf("Hola soy el hijo %d\n",getpid());
+    //espera a que se ejecute el proceso padre 
+     wait(NULL);       
+    //esta variable va ir almacenando cada caracter que se va a leer
+    //printf("la letra clave es: %c\n",letraClave);
+    char leer;
+    char* texto2;
+    read(padreHijo[0], texto2, sizeof(int));
+    printf("Hijo: Leído %c del pipe.\n", leer);
+    
+    for(int i = 0; i < sizeof(texto2); i++)
+    {
+         printf("%c\n", leer);
+            fputc(leer, archivoNuevo);
+    }
+    /*
+    //leo el fichero dandole valor a la variable leer y cuando llega al final 
+    //EOF  sale del bucle
+    while(leer != '1'){
+        //muestro por pantalla cada caracter
+        //printf("%c", leer);
+        
+            printf("%c\n", leer);
+            fputc(leer, archivoNuevo);
+            //wait(NULL);
+            sleep(1);
+    }
+    //sleep(1);
+   */
+//fin proceso hijo
+ }else if (pid == -1) {
     fprintf(stderr, "se a producido un error");
     exit(-1);
- } 
- else
- {
+ } else {//PROCESO PADRE lee y envia al hijo el caracter
     printf("Hola soy el padre %d, el hijo que he creado es %d\n",getpid(),pid);
     //con este if abro el documento de texto en la variable y compruebo que no este vacío o
     // que no lo encuentre
-    if ((archivo = fopen("texto.txt","r"))== NULL) {
+    if (archivo == NULL) {
         perror("la ruta del fichero es erronea");
     }
     char leer;
-    char[] texto;
+    char* texto;
+    int contador =0;  
     //leo el fichero dandole valor a la variable leer y cuando llega al final 
     //EOF  sale del bucle
+    leer=fgetc(archivo);
     while((leer=fgetc(archivo))!=EOF){
         //muestro por pantalla cada caracter
-        //printf("%c", leer);
-         write(padreHijo[1],&mensaje,sizeof(int));
-        }
-    
-    
-    
- }
-    
-    
-    archivoNuevo = fopen("texto_A.txt","w"); 
-        
-
-    //char letraClave = argv[2];
-    char letraClave = 'a';
-    //esta variable va ir almacenando cada caracter que se va a leer
-    printf("la letra clave es: %c\n",letraClave);
-    char leer;
-    //leo el fichero dandole valor a la variable leer y cuando llega al final 
-    //EOF  sale del bucle
-    while((leer=fgetc(archivo))!=EOF){
-        //muestro por pantalla cada caracter
-        //printf("%c", leer);
-        //comparo con el caracter que no quiero copiar y copio
-        if (leer != letraClave) {
-            printf("%c", leer);
-            fputc(leer, archivoNuevo);
-        }else  {
-            //printf("%c", leer);
-        }       
-        
-    }
-    printf("\n");
-    //cierro los ficheros
+        printf("%c\n", leer);
+        texto[contador] = leer;
+        contador++;
+         
+         //wait(NULL);
+         //sleep(1);
+        }    
+        //sizeof igual es char
+        write(padreHijo[1],&texto,sizeof(int));
+ }//fin padre
+       
     fclose(archivo);
     fclose(archivoNuevo);
 
